584df61c9d6e8c6788daeac57f90f5bced73901e
Moong0122 <2015004802@hanyang.ac.kr>
2020-01-30 14:53:30 +0900

    Updates

---
+++ precache-manifest.d0db262738dd5d7f7a925d075be72f8c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a4bcc2bcf83236569cbd709cc48e5b62",
+    "url": "/photobook-app/index.html"
+  },
+  {
+    "revision": "82f9baddda24c8dc9cd4",
+    "url": "/photobook-app/static/css/main.5ab4e9b0.chunk.css"
+  },
+  {
+    "revision": "51a95674768d6fdaff9b",
+    "url": "/photobook-app/static/js/2.15b82b3a.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/photobook-app/static/js/2.15b82b3a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "82f9baddda24c8dc9cd4",
+    "url": "/photobook-app/static/js/main.d1c28bb8.chunk.js"
+  },
+  {
+    "revision": "122565ce0673f2875837",
+    "url": "/photobook-app/static/js/runtime-main.573a9f15.js"
+  }
+]);
