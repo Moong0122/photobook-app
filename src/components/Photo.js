2279eb7c57f5e01dda34d7abe62c01c4322ea862
Moong0122 <venture1031@naver.com>
2020-08-26 17:31:48 +0900

    Photo 컴포넌트 + react spring

--- src/components/Photo.js
+++ src/components/Photo.js
@@ -1,9 +1,14 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import PropTypes from "prop-types";
+import { useSpring, animated } from 'react-spring';
 import "./Photo.css";
 
+const calc = (x,y) => [-(y-window.innerHeight / 2) / 20, (x-window.innerWidth / 2) / 20, 1.1];
+const trans = (x,y,s) => `perspective(600px) rotateX(${x}deg) rotateY(${y}deg) scale(${s})`;
+
 function Photo({ id, updated, image, name, bio, portfolio }) {
+  const [props, set] = useSpring(() => ({xys : [0,0,1], config: {mass:5, tension: 350, friction: 40}}));
   return (
     <div className="photo">
       <Link
@@ -12,7 +17,14 @@ function Photo({ id, updated, image, name, bio, portfolio }) {
           state: { updated, name, bio, portfolio }
         }}
       >
-        <img src={image} alt={name} />
+        <animated.div
+          class="card"
+          onMouseMove={({clientX :x, clientY:y}) => set({xys:calc(x,y)})}
+          onMouseLeave={()=>set({xys:[0,0,1] })}
+          style={{transform : props.xys.interpolate(trans)}}
+        >
+          <img src={image} alt={name} />
+        </animated.div>
       </Link>
     </div>
   );
